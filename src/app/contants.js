f69ea26e7ed26a41c3a5fa6114b58212d5051082
Avnishbharti <kaushlendra.pratap@corpseed.com>
2024-10-18 17:58:48 +0530

    login page Ui done

---
+++ src/app/contants.js
@@ -0,0 +1,2 @@
+export const ICON_HEIGHT=18
+export const ICON_WIDTH=18
---
+++ src/app/login/page.jsx
@@ -0,0 +1,66 @@
+"use client"
+import React from "react"
+import "./login.css"
+import { Button, Checkbox, Flex, Form, Input, Typography } from "antd"
+import hig from "../assets/lowZoom.png"
+import Image from "next/image"
+import Link from "next/link"
+import { Icon } from "@iconify/react"
+import { ICON_HEIGHT, ICON_WIDTH } from "../contants"
+const { Title, Text } = Typography
+
+const Login = () => {
+  return (
+    <Flex justify="center" align="center" className="login-container">
+      <Flex vertical gap={24} className="login-sub-container" align="center">
+        <Image src={hig} alt="lowzoom-logo" height={"10%"} width={"20%"} />
+        <Title className="main-heading-text" level={1}>
+          Sign in
+        </Title>
+        <Form size="large" style={{ width: "70%" }} layout='vertical'>
+          <Form.Item
+            label="Email"
+            name="username"
+            rules={[{ required: true, message: "Please enter your email id" }]}
+          >
+            <Input
+              prefix={
+                <Icon
+                  icon="fluent:mail-24-regular"
+                  height={ICON_HEIGHT}
+                  width={ICON_WIDTH}
+                />
+              }
+            />
+          </Form.Item>
+          <Form.Item
+            label="Password (min 8 character)"
+            name="password"
+            rules={[{ required: true, message: "Please enter your password" }]}
+          >
+            <Input
+              prefix={
+                <Icon
+                  icon="fluent:lock-closed-24-regular"
+                  height={ICON_HEIGHT}
+                  width={ICON_WIDTH}
+                />
+              }
+            />
+          </Form.Item>
+          <Form.Item valuePropName="checked">
+            <Checkbox>Remember me.</Checkbox>{" "}
+            <Link href={"/"}>Forget Password ?</Link>
+          </Form.Item>
+          <Form.Item>
+            <Button type="primary" htmlType="submit" style={{ width: "100%" }}>
+              Submit
+            </Button>
+          </Form.Item>
+        </Form>
+      </Flex>
+    </Flex>
+  )
+}
+
+export default Login
